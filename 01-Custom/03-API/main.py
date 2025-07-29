ea104b201b42eb2e65006835e74a949f28a56cf2
Ted Charles Brown <52015952+Tedcharlesbrown@users.noreply.github.com>
2025-07-29 15:02:06 -0400

    451

--- 01-Custom/03-API/main.py
+++ 01-Custom/03-API/main.py
@@ -9,36 +9,39 @@ if __name__ == "__main__":
 
     pixera = Pixera(ip, port)
     api = API()
+    
+    TIME_CUE_ATLANTA = 0.0
+    TIME_CUE_LOSANGELES = 1200.0
 
-    # ----------------------------- DEFAULT DEBUGGING ---------------------------- #
-    output = pixera.send()
+    handle_timeline1 = pixera.send(api.getTimelineAtIndex(0))
+    print(handle_timeline1)
+    # output = pixera.send(api.Timeline.applyCueWithName(timeline1, "First Scene", 0))
+    
+    
+    # ---------------------------- ENTER PREIVEW EDIT ---------------------------- #
+    # output = pixera.send(api.Timeline.startPreviewEdit(handle_timeline1, TIME_CUE_LOSANGELES), verbose=True)
+    # sleep(1)
+    
+    # ----------------------- SET ANOTHER PREVIEW POSITION ----------------------- #
+    # output = pixera.send(api.Timeline.startPreviewEdit(handle_timeline1, TIME_CUE_ATLANTA), verbose=True)
+    # sleep(1)
+    
+    # -------------------------- TRY TO MOVE NOWPOINTER -------------------------- #
+    # output = pixera.send(api.Timeline.setCurrentTime(handle_timeline1, TIME_CUE_ATLANTA), verbose=True)
+    # sleep(1)
+    
+    # ----------------------------- END PREVIEW EDIT ----------------------------- #
+    output = pixera.send(api.Timeline.endPreviewEdit(handle_timeline1, True, 1000))
+    print(f"End Preview Edit Output: {output}")
+    sleep(2)
+    output = pixera.send(api.Timeline.play(handle_timeline1), verbose=True)
     print(output)
-    sleep(1)
-    # ----------------------------- VERBOSE DEBUGGING ---------------------------- #
-    output = pixera.send(api.outputDebug("Hello from Python!"), verbose=True)
-    print(output)
-    sleep(1)
-    # ---------------------------- CHANGING API METHOD --------------------------- #
-    output = pixera.send(api.getApiRevision(), protocol="TCP_DL", port=1401, verbose=True)
-    print(output)
-    sleep(1)
-    # ------------------------- ACCESSING CONTROL MODULES ------------------------ #
-    output = pixera.send(["NewModule.Test"])
-    print(output)
-    sleep(1)
-    # ---------------------------- MULTIPLE PARAMETERS --------------------------- #
-    pixera.send(api.setTransportModeOnTimeline("Timeline 1", 1))
-    sleep(1)
-    pixera.send(api.setTransportModeOnTimeline("Timeline 1", 0))
-    sleep(1)
-    # ----------------------------- HANDLING HANDLES ----------------------------- #
-    screens = pixera.return_array(pixera.send(api.getScreens()))
-    for screen in screens:
-        output = pixera.send(api.Screen().getName(int(screen)), verbose=True)
-        print(output)
-        sleep(1)
-
-
-    # ------------------------------- MANUAL INPUT ------------------------------- #
-    output = pixera.send(["Pixera.Utility.outputDebug", ["message"], ["Hello from Python!"]], verbose=True)
-    print(output)
+    # sleep(1)
+    
+    # current_time = pixera.send(api.Timeline.getCurrentTime(handle_timeline1))
+    # print(f"Current Time: {current_time}")
+    
+    # print(output)
+    
+    # current_time = pixera.send(api.Timeline.getCurrentTime(timeline1))
+    # print(f"Current Time: {current_time}")
